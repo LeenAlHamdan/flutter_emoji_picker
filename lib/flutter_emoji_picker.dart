2bd85d139aec01cb10007cd82d11355d7b91d407
Leen Alhamdan <leenalhamdan@gmail.com>
2022-08-02 20:38:23 +0300

    first commit

---
+++ lib/flutter_emoji_picker.dart
@@ -0,0 +1,15 @@
+library flutter_emoji_picker;
+
+export 'package:flutter_emoji_picker/src/category_icons.dart';
+export 'package:flutter_emoji_picker/src/config.dart';
+export 'package:flutter_emoji_picker/src/emoji.dart';
+export 'package:flutter_emoji_picker/src/emoji_picker.dart';
+export 'package:flutter_emoji_picker/src/emoji_picker_builder.dart';
+export 'package:flutter_emoji_picker/src/emoji_picker_utils.dart';
+export 'package:flutter_emoji_picker/src/emoji_view_state.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/src/category_emoji.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_emoji_picker/flutter_emoji_picker.dart';
+
+/// Container for Category and their emoji
+class CategoryEmoji {
+  /// Constructor
+  CategoryEmoji(this.category, this.emoji);
+
+  /// Category instance
+  final Category category;
+
+  /// List of emoji of this category
+  List<Emoji> emoji;
+}
